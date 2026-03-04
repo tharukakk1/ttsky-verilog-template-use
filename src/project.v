943cd8a910758f613f4ae9907e508af8c63a5422
Tharuka Kodituwakku <106564811+tharukakk1@users.noreply.github.com>
2026-03-03 22:08:29 -0800

    project.v copied from my fork project

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_tharukakk1 (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,12 +16,17 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  // // All output pins must be assigned. If not used, assign to 0.
+  // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  // assign uio_out = 0;
+  // assign uio_oe  = 0;
 
-  // List all unused inputs to prevent warnings
+  // // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
+  
+  wire [8:0] full_result = ui_in + uio_in;
+  assign uo_out  = full_result[7:0];
+  assign uio_out = {7'b0, full_result[8]};
+  assign uio_oe  = 8'b00000001;
 
 endmodule
